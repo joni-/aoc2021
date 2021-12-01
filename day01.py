0f5fc5555bb5fc81084137979c48ac08c05df9ba
Joni Nevalainen <joninevalainen@gmail.com>
2021-12-01 08:25:57 +0200

    Day 1

--- day01.py
+++ day01.py
@@ -1,12 +1,33 @@
 from common import read_input
 
 
-def part1(input: str) -> str:
-    return "123"
+def part1(input: str) -> int:
+    ns = [int(n) for n in input.splitlines()]
+    pairs = zip(ns, ns[1:])
+
+    increments = 0
+    for a, b in pairs:
+        if a < b:
+            increments += 1
+
+    return increments
 
 
-def part2(input: str) -> str:
-    return "123"
+def part2(input: str) -> int:
+    ns = [int(n) for n in input.splitlines()]
+    chunks = []
+
+    for i in range(len(ns)):
+        chunks.append(sum(ns[i : i + 3]))
+
+    pairs = zip(chunks, chunks[1:])
+
+    increments = 0
+    for a, b in pairs:
+        if a < b:
+            increments += 1
+
+    return increments
 
 
 if __name__ == "__main__":
--- tests/test_day01.py
+++ tests/test_day01.py
@@ -1,5 +1,31 @@
-from day01 import part1
+from day01 import part1, part2
 
 
 def test_day01_part1():
-    assert part1("") == "123"
+    input = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
+    assert part1(input) == 7
+
+
+def test_day01_part2():
+    input = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
+    assert part2(input) == 5
