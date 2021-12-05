5364615c48ec351c35cb1f0c954e8bd521a82a91
Joni Nevalainen <joninevalainen@gmail.com>
2021-12-05 10:50:30 +0200

    Day 5

---
+++ day05.py
@@ -0,0 +1,91 @@
+import re
+from collections import defaultdict, namedtuple
+
+from common import count_by, read_input_lines
+
+Point = namedtuple("Point", ["x", "y"])
+line_regex = re.compile(r"^(\d+),(\d+) -> (\d+),(\d+)$")
+
+
+def overlaps(item: tuple[Point, int]) -> bool:
+    return item[1] >= 2
+
+
+count_overlapping_points = count_by(overlaps)  # type: ignore
+
+
+def _parse_lines(input: list[str]) -> list[tuple[Point, Point]]:
+    lines: list[tuple[Point, Point]] = []
+    for line in input:
+        m = re.match(line_regex, line)
+        if m:
+            x1, y1, x2, y2 = [int(x) for x in m.groups()]
+            lines.append((Point(x1, y1), Point(x2, y2)))
+    return lines
+
+
+def part1(input: list[str]) -> int:
+    lines = [
+        (start, end)
+        for start, end in _parse_lines(input)
+        if start.x == end.x or start.y == end.y
+    ]
+
+    counts: dict[Point, int] = defaultdict(int)
+
+    for start, end in lines:
+        for x in range(min(start.x, end.x), max(start.x, end.x) + 1):
+            for y in range(min(start.y, end.y), max(start.y, end.y) + 1):
+                counts[Point(x, y)] = counts[Point(x, y)] + 1
+
+    # _draw(_parse_lines(input), counts)
+    return count_overlapping_points(counts.items())  # type: ignore
+
+
+def part2(input: list[str]) -> int:
+    lines = [
+        (start, end)
+        for start, end in _parse_lines(input)
+        if start.x == end.x or start.y == end.y
+    ]
+    diagonals = [
+        (start, end)
+        for start, end in _parse_lines(input)
+        if start.x != end.x and start.y != end.y
+    ]
+
+    counts: dict[Point, int] = defaultdict(int)
+
+    for start, end in lines:
+        for x in range(min(start.x, end.x), max(start.x, end.x) + 1):
+            for y in range(min(start.y, end.y), max(start.y, end.y) + 1):
+                counts[Point(x, y)] = counts[Point(x, y)] + 1
+
+    for start, end in diagonals:
+        for step in range(max(start.x, end.x) - min(start.x, end.x) + 1):
+            point = Point(
+                start.x - step if start.x > end.x else start.x + step,
+                start.y - step if start.y > end.y else start.y + step,
+            )
+            counts[point] = counts[point] + 1
+
+    # _draw(_parse_lines(input), counts)
+    return count_overlapping_points(counts.items())  # type: ignore
+
+
+def _draw(lines: list[tuple[Point, Point]], counts: dict[Point, int]):
+    max_x = max([max(start.x, end.x) for start, end in lines])
+    max_y = max([max(start.y, end.y) for start, end in lines])
+
+    for y in range(max_y + 1):
+        row = ""
+        for x in range(max_x + 1):
+            mark = counts[Point(x, y)] or "."
+            row += str(mark)
+        print(row)
+
+
+if __name__ == "__main__":
+    data_in = read_input_lines(5, str)
+    print(part1(data_in))
+    print(part2(data_in))
---
+++ tests/test_day05.py
@@ -0,0 +1,22 @@
+from day05 import part1, part2
+
+input = [
+    "0,9 -> 5,9",
+    "8,0 -> 0,8",
+    "9,4 -> 3,4",
+    "2,2 -> 2,1",
+    "7,0 -> 7,4",
+    "6,4 -> 2,0",
+    "0,9 -> 2,9",
+    "3,4 -> 1,4",
+    "0,0 -> 8,8",
+    "5,5 -> 8,2",
+]
+
+
+def test_day05_part1():
+    assert part1(input) == 5
+
+
+def test_day05_part2():
+    assert part2(input) == 12
