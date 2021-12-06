3b1b2fcc68a0c30d2003a8b10ba5f4ab1b3ac2a8
Joni Nevalainen <joninevalainen@gmail.com>
2021-12-06 09:19:23 +0200

    Day 6

---
+++ day06.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+from common import read_input
+
+
+def _count_fishes(input: list[int], days: int) -> int:
+    counts: dict[int, int] = defaultdict(int)
+
+    for n in input:
+        counts[n] = counts[n] + 1
+
+    for _ in range(days):
+        new_counts: dict[int, int] = defaultdict(int)
+        for days_left, fish_count in counts.items():
+            if days_left != 0:
+                new_counts[days_left - 1] = fish_count
+        new_counts[6] = new_counts[6] + counts[0]
+        new_counts[8] = counts[0]
+        counts = new_counts
+    return sum(counts.values())
+
+
+def part1(input: list[int]) -> int:
+    return _count_fishes(input, 80)
+
+
+def part2(input: list[int]) -> int:
+    return _count_fishes(input, 256)
+
+
+if __name__ == "__main__":
+    data_in = [int(x) for x in read_input(6).split(",")]
+    print(part1(data_in))
+    print(part2(data_in))
---
+++ tests/test_day06.py
@@ -0,0 +1,11 @@
+from day06 import part1, part2
+
+input = [3, 4, 3, 1, 2]
+
+
+def test_day06_part1():
+    assert part1(input) == 5934
+
+
+def test_day06_part2():
+    assert part2(input) == 26984457539
